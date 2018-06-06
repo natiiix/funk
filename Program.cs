7645edb77c4908c81d1f2af13dc4ff65ff151192
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-07 00:58:06 +0200

    Implement core functionality

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace funk
+namespace Funk
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // No arguments provided
+            // Print the expected syntax
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Syntax: funk <source file> [<source file> ...]");
+                Environment.Exit(0);
+            }
+
+            // Load all the source files
+            List<string> sourceFiles = new List<string>();
+
+            foreach (string filePath in args)
+            {
+                // File exists
+                // Read it
+                if (File.Exists(filePath))
+                {
+                    sourceFiles.Add(File.ReadAllText(filePath));
+                }
+                // File does not exist
+                // Print an error and stop execution
+                else
+                {
+                    ExitWithError($"Source file \"{filePath}\" does not exist");
+                }
+            }
+
+            // TODO
+        }
+
+        public static void ExitWithError(string errorMessage, int errorCode = -1)
+        {
+            Console.Error.WriteLine("ERROR: " + errorMessage);
+            Environment.Exit(-1);
         }
     }
 }
