d720a8eb432d033caf7cab1f056ff1329a6e4eeb
Ivo Meixner <ivo.meixner@se7en.cz>
2018-06-07 10:27:52 +0200

    Add ToString method overload to Token for easier debugging

--- Token.cs
+++ Token.cs
@@ -5,10 +5,24 @@ namespace Funk
         public readonly TokenType Type;
         public readonly string Value;
 
-        public Token(TokenType type, string value)
+        public Token(TokenType type, string value = "")
         {
             Type = type;
             Value = value;
+        }
+
+        public override string ToString()
+        {
+            string str = $"Token({Type}";
+
+            if (Value.Length > 0)
+            {
+                str += $", \"{Value}\"";
+            }
+
+            str += ")";
+
+            return str;
         }
     }
 }
